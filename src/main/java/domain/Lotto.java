a686b7399bc21ad237ea521ac964333d87684556
opdshe <50160282+opdshe@users.noreply.github.com>
2020-04-11 16:32:58 +0900

    랜덤변수 생성, 로또 리스트 초기화

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -6,12 +6,31 @@ import java.util.List;
  * 로또 한장을 의미하는 객체
  */
 public class Lotto {
+    private final int RANDOM_MAX = 45;
+    private final int RANDOM_MIN = 1;
+    private final int LOTTO_LENGTH = 6;
 
     private final List<Integer> numbers;
 
     public Lotto(final List<Integer> numbers) {
         this.numbers = numbers;
+        createNumbers();
     }
 
-    // 추가 기능 구현
+    public void addRandomNum(int randomNumber) {
+        if (!numbers.contains(randomNumber)) {
+            numbers.add(randomNumber);
+        }
+    }
+
+    public void createNumbers() {
+        while (numbers.size() < LOTTO_LENGTH) {
+            int randomNumber = (int) (Math.random() * RANDOM_MAX + RANDOM_MIN);
+            addRandomNum(randomNumber);
+        }
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
 }
--- src/main/java/lotto/App.java
+++ src/main/java/lotto/App.java
@@ -1,11 +1,16 @@
 package lotto;
 
+import domain.Lotto;
+
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
     private static final int LOTTO_PRICE = 1000;
     private static final Scanner sc = new Scanner(System.in);
+    private static final List<Lotto> lottos = new ArrayList<>();
     private static int purchasingAmount;
     private static boolean status;
 
@@ -26,14 +31,24 @@ public class App {
             System.out.println("구입 금액을 입력하세요.");
             try {
                 status = checkPurchasingAmount(purchasingAmount = sc.nextInt());
-            }catch (InputMismatchException e ){
+            } catch (InputMismatchException e) {
                 System.out.println("입력은 정수로만 가능합니다.");
                 sc.next(); // Scanner 버그 방지용 코드
             }
         } while (!status);
     }
 
+    public static void initLottos() {
+        int numOfLotto = purchasingAmount / LOTTO_PRICE;
+        System.out.println(numOfLotto + "개를 구매했습니다.");
+        for (int i = 0; i < numOfLotto; i++) {
+            lottos.add(new Lotto(new ArrayList<>()));
+        }
+    }
+
     public static void main(String[] args) {
         setPurchasingAmount();
+        initLottos();
+        lottos.forEach(lotto -> System.out.println(lotto.getNumbers().toString()));
     }
 }
