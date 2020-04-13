740ab4da8c90932992b31cb0099b29e6268df0ae
taeeun kim <kimtaeeeny@gmail.com>
2020-04-13 15:28:25 +0900

    구현 완료

--- src/main.js
+++ src/main.js
@@ -39,22 +39,41 @@ const setLotto = (lottoCount, lottos) => {
   }
 }
 
-const matchLotto = (myLottos, winningLotto) => {
+const matchLottos = (myLottos, winningLotto) => {
   for (let i = 0; i < myLottos.length; i++) {
-    if (winningLotto.match(myLottos[i]) === 1) {
-      obj['FIRST']++
-    } else if (winningLotto.match(myLottos[i]) === 2) {
-      obj['SECOND']++
-    } else if (winningLotto.match(myLottos[i]) === 3) {
-      obj['THIRD']++
-    } else if (winningLotto.match(myLottos[i]) === 4) {
-      obj['FOURTH']++
-    } else if (winningLotto.match(myLottos[i]) === 5) {
-      obj['FIFTH']++
-    } else {
-      obj['MISS']++
-    }
+    matchOneLotto(i, myLottos, winningLotto)
   }
+}
+
+const matchOneLotto = (i, myLottos, winningLotto) => {
+  if (winningLotto.match(myLottos[i]) === 1) {
+    obj['FIRST']++
+  } else if (winningLotto.match(myLottos[i]) === 2) {
+    obj['SECOND']++
+  } else if (winningLotto.match(myLottos[i]) === 3) {
+    obj['THIRD']++
+  } else if (winningLotto.match(myLottos[i]) === 4) {
+    obj['FOURTH']++
+  } else if (winningLotto.match(myLottos[i]) === 5) {
+    obj['FIFTH']++
+  } else {
+    obj['MISS']++
+  }
+}
+
+const checkInputValue = (lottos, bonusBall) => {
+  let tempSet = new Set()
+  lottos = lottos.split(',')
+  if (lottos.length !== 6) return false
+  if (bonusBall > 45 || bonusBall <= 0) return false
+  lottos.map((item) => {
+    if (item > 45 || item <= 0) return false
+  })
+  for (let i = 0; i < lottos.length; i++) {
+    tempSet.add(lottos[i])
+  }
+  if (tempSet.length !== 6) return false
+  return true
 }
 
 const priceForm = document.createElement('form')
@@ -76,14 +95,14 @@ const conditionForm = document.createElement('form')
 conditionForm.style.display = 'none'
 body.append(conditionForm)
 
-makeDescription('지난주. ', conditionForm)
+makeDescription('<br/>지난주 당첨 번호를 입력해주세요.', conditionForm)
 
 const lottoNumberInput = document.createElement('input')
 lottoNumberInput.type = 'text'
 lottoNumberInput.style.width = '500px'
 conditionForm.append(lottoNumberInput)
 
-makeDescription('보너스볼. ', conditionForm)
+makeDescription('보너스볼을 입력해주세요.', conditionForm)
 
 const bonusBallInput = document.createElement('input')
 bonusBallInput.type = 'text'
@@ -98,16 +117,16 @@ conditionForm.append(conditionButton)
 priceForm.addEventListener('submit', (e) => {
   e.preventDefault()
   if (!getLottoCount(priceInput.value)) {
-    makeDescription('에러. ', priceForm)
+    makeDescription('입력값이 잘못되었습니다.', priceForm)
     return
   }
   const lottoCount = getLottoCount(priceInput.value)
   setLotto(lottoCount, myLottos)
   let result = ''
-  makeDescription(`${lottoCount}개를 구매했습니다.`, priceForm)
+  makeDescription(`<br/>${lottoCount}개를 구매했습니다.`, priceForm)
   for (let i = 0; i < lottoCount; i++) {
     console.log(myLottos[i])
-    result += `${myLottos[i].numbers}<br/>`
+    result += `[${myLottos[i].numbers.join(', ')}]<br/>`
   }
   makeDescription(`${result}`, priceForm)
   conditionForm.style.display = 'block'
@@ -115,10 +134,29 @@ priceForm.addEventListener('submit', (e) => {
 
 conditionForm.addEventListener('submit', (e) => {
   e.preventDefault()
+  if (!checkInputValue(lottoNumberInput.value, bonusBallInput.value)) {
+    makeDescription('입력값이 잘못되었습니다.', conditionForm)
+    return
+  }
   const winningLotto = new WinningLotto(
     new Lotto(lottoNumberInput.value.split(',').map((item) => Number(item))),
     Number(bonusBallInput.value)
   )
-  matchLotto(myLottos, winningLotto)
-  console.log(obj)
+  matchLottos(myLottos, winningLotto)
+  let result = '<br/>당첨통계<br/>--------<br/>'
+  const fifthText = `3개일치(5000원)- ${obj['FIFTH']}개<br/>`
+  const fourthText = `4개일치(50000원)- ${obj['FOURTH']}개<br/>`
+  const thirdText = `5개일치(1500000원)- ${obj['THIRD']}개<br/>`
+  const secondText = `5개일치, 보너스 볼 일치(30000000원)- ${obj['SECOND']}개<br/>`
+  const firstText = `6개일치(2000000000원)- ${obj['FIRST']}개<br/>`
+  const totalSum =
+    obj['FIFTH'] * 5000 +
+    obj['FOURTH'] * 50000 +
+    obj['THIRD'] * 1500000 +
+    obj['SECOND'] * 30000000 +
+    obj['FIRST'] * 2000000000
+  const lastText = `총 수익률은 ${totalSum / priceInput.value}입니다.`
+  result += fifthText + fourthText + thirdText + secondText + firstText + lastText
+
+  makeDescription(result, conditionForm)
 })
