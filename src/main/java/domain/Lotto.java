e2672e01759ee21915ae721821ae5a65dda2d433
eunjeongS2 <dmswjd2340@gmail.com>
2020-04-16 15:49:39 +0900

    로또 개수만큼 6개씩 랜덤 번호 뽑기 구현

--- src/main/java/domain/Lotto.java
+++ src/main/java/domain/Lotto.java
@@ -1,11 +1,15 @@
 package domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Random;
 
 /**
  * 로또 한장을 의미하는 객체
  */
 public class Lotto {
+    private static final int RANGE = 45;
+    private static final int SELECT_NUMBER_LENGTH = 6;
 
     private final List<Integer> numbers;
 
@@ -13,5 +17,18 @@ public class Lotto {
         this.numbers = numbers;
     }
 
-    // 추가 기능 구현
+    public void randomNumbers() {
+        Random random = new Random();
+        HashSet<Integer> randomSet = new HashSet<>();
+
+        while (randomSet.size() < SELECT_NUMBER_LENGTH) {
+            randomSet.add(random.nextInt(RANGE) + 1);
+        }
+        numbers.addAll(randomSet);
+    }
+
+    public void printNumbers() {
+        System.out.println(numbers);
+    }
+
 }
--- src/main/java/domain/Program.java
+++ src/main/java/domain/Program.java
@@ -2,6 +2,8 @@ package domain;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Program {
     private static final int PRICE = 1000;
@@ -10,6 +12,8 @@ public class Program {
 
     public static void main(String[] args) throws Exception {
         int purchaseCount = inputPrice() / PRICE;
+        List<Lotto> lottoList = setupLottoList(purchaseCount);
+        printLottoList(lottoList);
     }
 
     private static int inputPrice() throws Exception {
@@ -19,6 +23,24 @@ public class Program {
     private static int inputInt(String print) throws Exception {
         System.out.println(print);
         return Integer.parseInt(br.readLine());
+    }
+
+    private static List<Lotto> setupLottoList(int count) {
+        List<Lotto> lottoList = new ArrayList<>();
+        while (count != 0) {
+            Lotto lotto = new Lotto(new ArrayList<>());
+            lotto.randomNumbers();
+            lottoList.add(lotto);
+            count--;
+        }
+        return lottoList;
+    }
+
+    private static void printLottoList(List<Lotto> lottoList) {
+        System.out.println(lottoList.size() + "개를 구매했습니다");
+        for (Lotto lotto : lottoList) {
+            lotto.printNumbers();
+        }
     }
 
 }
